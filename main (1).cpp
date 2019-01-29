8fc8bb9eb87ae02ba5b016c5abcad1b192d38122
Curtis-96 <curtisfraserteitleman@gmail.com>
2019-01-28 23:01:59 -0500

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+}
+
+unsigned long hash(unsigned char *str)
+{
+  unsigned long hash=5381;
+  int c;
+  while ((c=*str++)) hash=((hash << 5) + hash) + c;
+  //hash*33+c
+  return hash;
+}
+//function returns index
+//has to hash from key type to integer type
+// hash table is simply an array of linked lists
+//hash table has keys and elements
+
+class hash
+{
+public:
+  get_offset()
+{
+
+};
+set_offset()
+{
+  int offset;
+  hash%table=offset;
+
+  for(offset>5381)
+  {
+    std::cout << "There is not enough space in hashtable" << std::endl;
+  }
+};
+
+
+};
